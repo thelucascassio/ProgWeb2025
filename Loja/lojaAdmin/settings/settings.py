a9d9db68e69154ab2e1bb05a95ca9dfd8c31853d
Lucas Cássio Araújo Oliveira <20231011110002@ifrn.local>
2025-04-14 15:50:26 -0300

    Loja atualizada, aplicação realizada em sala de aula

--- Loja/lojaAdmin/settings/settings.py
+++ Loja/lojaAdmin/settings/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+# abaixo do BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao-Paulo'
 
 USE_I18N = True
 
