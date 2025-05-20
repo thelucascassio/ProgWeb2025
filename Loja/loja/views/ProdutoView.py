425019dfd3af59c72396717f3a4a00199d1b00bb
thelucascassio <lucask4ssio@gmail.com>
2025-05-20 14:04:43 -0300

    Atualização das configurações de filtragem no Django

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from loja.models import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
 
 def list_produto_view(request, id=None):
     #carrega informações vindas do navegador (query string)
@@ -8,6 +10,7 @@ def list_produto_view(request, id=None):
     promocao = request.GET.get("promocao")
     categoria = request.GET.get("categoria")
     fabricante = request.GET.get("fabricante")
+    dias = request.GET.get("dias")
     #mostra no console
     if destaque is not None:
         print(destaque)
@@ -23,17 +26,21 @@ def list_produto_view(request, id=None):
     #carrega informações vindas do banco de dados
     produtos = Produto.objects.all()
     if produto is not None:
-        produtos = produtos.filter(Produto=produto)
+        produtos = produtos.filter(Produto__contains=produto )
     if promocao is not None:
         produtos = produtos.filter(promocao=promocao)
     if destaque is not None:
         produtos = produtos.filter(destaque=destaque)
     if categoria is not None:
-        produtos = produtos.filter(categoria=categoria)
+        produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
-        produtos = produtos.filter(fabricante=fabricante)
+        produtos = produtos.filter(fabricante__Fabricante=fabricante) #esses underscores fazem a comparação do id da categoria com o texto referente ao nome da CATEGORIA na TABELA
     if id is not None:
         produtos = produtos.filter(id=id)
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
     
     #mostra no console
     print(produtos)
