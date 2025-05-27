95d82c9d9efcece1cfd719c077b7a138ad6be723
thelucascassio <lucask4ssio@gmail.com>
2025-05-27 14:26:47 -0300

    início dos ajustes de templates

--- Loja/loja/views/HomeView.py
+++ Loja/loja/views/HomeView.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse('<h1>Olá mundo!</h1>')
+    #return HttpResponse('<h1>Olá mundo!</h1>')
+    return render(request, template_name='home/home.html', status=200)
