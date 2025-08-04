a749b9fcfb40cfa4fa5ba6cf76a441ad52d9a8eb
Lucas Cássio Araújo Oliveira <20231011110002@ifrn.local>
2025-08-04 15:53:05 -0300

    Início do capítulo de Forms

--- Loja/loja/views/UsuarioView.py
+++ Loja/loja/views/UsuarioView.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
+from loja.forms.UserUsuarioForm import UserUsuarioForm
 def list_usuario_view(request, id=None):
     # carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
@@ -8,3 +9,11 @@ def list_usuario_view(request, id=None):
     }
     return render(request, template_name='usuario/usuario.html', context=context,
     status=200)
+
+def edit_usuario_view(request):
+    usuario = get_object_or_404(Usuario, user=request.user)
+    usuarioForm = UserUsuarioForm(instance=usuario)
+    context = {
+    'usuarioForm': usuarioForm
+    }
+    return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
