5fce5c44eccac58603bdb70bfafcb09dd8f506db
Lucas Cássio Araújo Oliveira <20231011110002@ifrn.local>
2025-08-12 14:11:39 -0300

    Finalização do capítulo 9

--- Loja/loja/views/UsuarioView.py
+++ Loja/loja/views/UsuarioView.py
@@ -12,6 +12,8 @@ def list_usuario_view(request, id=None):
 
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
+    emailUnused = True
+    message = None
     if request.method == 'POST':
         usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
         userForm = UserForm(request.POST, instance=request.user)
@@ -20,11 +22,19 @@ def edit_usuario_view(request):
     else:
         usuarioForm = UserUsuarioForm(instance=usuario)
         userForm = UserForm(instance=request.user)
-        if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
-            usuarioForm.save()
-            userForm.save()
+    if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
+        usuarioForm.save()
+        userForm.save()
+        message = { 'type': 'success', 'text': 'Dados atualizados com sucesso' }
+    else:
+        if request.method == 'POST':
+            if emailUnused:
+                message = { 'type': 'danger', 'text': 'Dados inválidos' }
+            else:
+                message = { 'type': 'warning', 'text': 'E-mail já usado' }
     context = {
     'usuarioForm': usuarioForm,
-    'userForm': userForm
+    'userForm': userForm,
+    'message': message
     }
     return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
